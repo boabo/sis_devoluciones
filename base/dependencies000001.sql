18b5694acd8d788c5ae3b95106c1964ee67a6c3f
finguer <fabiofigueroap@gmail.com>
2021-03-01 12:31:25 -0400

    agregar nota agencia en el menu

--- base/dependencies000001.sql
+++ base/dependencies000001.sql
@@ -6,5 +6,6 @@ select pxp.f_insert_testructura_gui ('TDOCLIQ', 'LIQFILE');
 select pxp.f_insert_testructura_gui ('SCREENLIQ', 'LIQFILE');
 select pxp.f_insert_testructura_gui ('SCREENDESL', 'LIQFILE');
 select pxp.f_insert_testructura_gui ('GN2', 'DECR');
+select pxp.f_insert_testructura_gui ('RGA', 'DECR');
 
 /********************************************F-DEP-FFP-DECR-0-07/07/2020********************************************/
